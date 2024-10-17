b5864f2adacbee69f19c06d1a6aca50820d8acd4
Mujee125 <mujo6999@gmail.com>
2024-10-17 14:48:52 +0500

    boiler plate for paper app created

---
+++ src/electron/resourceManager.ts
@@ -0,0 +1,57 @@
+const POLLING_INTERVAL = 500;
+import osUtils from "os-utils";
+import fs from "fs";
+import os from "os";
+import { BrowserWindow } from "electron";
+
+export function pollResources(mainWindow: BrowserWindow) {
+  setInterval(async () => {
+    //   getting ⁡⁢⁣⁢​‌‍‌CPU​⁡ usage
+    const cpuUsage = await getCpuUsage();
+    // getting ‍‍⁡⁢⁣⁢‍RAM​⁡ usage
+    const ramUsage = getRamUsage();
+
+    // getting ⁡⁢⁣⁢​‌‍‌Storage Data​⁡
+
+    const storageData = getStorageData();
+
+    mainWindow.webContents.send("statistics", {
+      cpuUsage,
+      ramUsage,
+      storageUsage: storageData.usage,
+    });
+  }, POLLING_INTERVAL);
+}
+
+export function getStaticData() {
+  const totalStorage = getStorageData().total;
+  const cpuModel = os.cpus()[0].model;
+  const totalMemoryGB = Math.floor(osUtils.totalmem() / 1024);
+
+  return {
+    totalStorage,
+    cpuModel,
+    totalMemoryGB,
+  };
+}
+
+function getCpuUsage() {
+  return new Promise((resolve) => {
+    osUtils.cpuUsage(resolve);
+  });
+}
+
+function getRamUsage() {
+  return 1 - osUtils.freememPercentage();
+}
+
+function getStorageData() {
+  const stats = fs.statfsSync(process.platform === "win32" ? "C://" : "/");
+  const total = stats.bsize * stats.blocks;
+  const free = stats.bsize * stats.bfree;
+
+  return {
+    total: Math.floor(total / 1_000_000_000),
+    usage: 1 - free / total,
+  };
+}
